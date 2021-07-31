f195686f3c48306f9f3b4eaf88c3339520e5cca0
Jamaal Lawson <maaliemaal@gmail.com>
2021-07-31 09:46:54 -0400

    added a function that will add a engineer after being prompted

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 //import manager,engineer, and intern classes
-const Manager = require('Manager');
-const Engineer = require('Engineer');
-const Intern = require('Intern');
+const manager = require('Manager');
+const engineer = require('Engineer');
+const intern = require('Intern');
+const renderOutPut = require('./dist/renderOutput');
+let teamMember = [];
+
 
 /* WHEN I start the application
 THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
@@ -32,13 +35,100 @@ const promptUser = () => {
         {
             type: "input",
             name: "enter managers email",
-            default: "Table of contents?"
+            message: "Table of contents?"
         },
         {
             type: "input",
             name: "officeNum",
             message: "enter managers office number"
         },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "add team member",
+            choices: ["engineer", "intern"]
+        },
 ])
 
 };
+
+const init = () => {
+    promptUser()
+        .then((data) =>
+            fs.writeFile('teamProfile.HTML', renderOutPut(data), (err) =>
+                err ? console.error(err) : console.log('HTML Team Profile was successfully created!!')));
+};
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer",
+            message: "What is the engineers name?"
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "enter your engineer id"
+        },
+        {
+            type: "input",
+            name: "engineersEmail",
+            message: "enter engineers email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "enter engineers github"
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "add team member",
+            choices: ["engineer", "intern"],
+        },
+])
+.then(data => {
+    let engineer = new Engineer(data.engineer, data.engineerID, data.engineersEmail, data.github);
+    teamMember.push(engineer);
+})
+
+};
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "intern",
+            message: "What is the interns name?"
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "enter your intern id"
+        },
+        {
+            type: "input",
+            name: "internsEmail",
+            message: "enter interns email"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "enter interns school"
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "add team member",
+            choices: ["intern", "intern"],
+        },
+])
+.then(data => {
+    let engineer = new Engineer(data.engineer, data.engineerID, data.engineersEmail, data.github);
+    teamMember.push(engineer);
+})
+
+};
+
+init();
