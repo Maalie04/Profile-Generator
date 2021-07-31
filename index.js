64f15b0491f9c780a9139da07175476e0b88fb78
Jamaal Lawson <maaliemaal@gmail.com>
2021-07-31 02:19:10 -0400

    get role function returns the role for the intern, engineer, and manager

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+//import inquirer package
 const inquirer = require('inquirer');
+//import manager,engineer, and intern classes
+const Manager = require('Manager');
+const Engineer = require('Engineer');
+const Intern = require('Intern');
+
